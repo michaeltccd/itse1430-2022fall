1310e1fe87fc8b9824818e23d6555fa225392b5c
michael.taylor <michael.taylor@my.tccd.edu>
2022-08-24 21:36:10 -0500

    Hello world and section 1.

---
+++ classwork/HelloWrold/HelloWrold/Program.cs
@@ -0,0 +1,22 @@
+﻿//
+// Your Name
+// ITSE 1430 2022 Fall
+// Lab 1
+//
+namespace HelloWorld
+{
+    /*
+     * A multiline comment
+     */
+    internal class Program 
+    {
+        // A comment
+        static void Main ( string[] args )
+        { 
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            //Another block of code
+        }
+    }
+}
