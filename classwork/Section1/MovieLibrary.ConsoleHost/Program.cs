6c1eab4c20c882bc64c4341be229d4156b673f1a
michael.taylor <michael.taylor@my.tccd.edu>
2022-08-31 21:49:08 -0500

    Implemented code to read strings and ints. Refactored to using functions.

--- classwork/Section1/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/Section1/MovieLibrary.ConsoleHost/Program.cs
@@ -6,14 +6,47 @@ int releaseYear = 1900;
 string rating = "";
 bool isClassic = false;
 
-//string title = "";
-Console.WriteLine("Enter a title: ");
+AddMovie();
 
-//string description = "";
-Console.WriteLine("Enter an optional description: ");
+int ReadInt32 ( string message )
+{
+    Console.Write(message);
 
-//int runLength = 0; //in minutes
-Console.WriteLine("Enter a run length (in minutes): ");
-Console.WriteLine("Enter the release year: ");
-Console.WriteLine("Entering MPAA rating: ");
-Console.WriteLine("Is this a classic? ");
+    string value = Console.ReadLine();
+
+    //Inline variable declarations    
+    //int result;
+    //if (Int32.TryParse(value, out result))
+    if (Int32.TryParse(value, out int result))
+        return result;
+
+    //TODO: Loop
+    return -1;
+}
+
+string ReadString ( string message )
+{
+    //message = "Bob";
+    Console.Write(message);
+
+    string value = Console.ReadLine();
+
+    return value;
+}
+
+void AddMovie ()
+{
+    //string title = "";
+    title = ReadString("Enter a title: ");
+
+    //string description = "";
+    description = ReadString("Enter an optional description: ");
+
+    //int runLength = 0; //in minutes
+    runLength = ReadInt32("Enter a run length (in minutes): ");
+
+    releaseYear = ReadInt32("Enter the release year: ");
+    rating = ReadString("Entering MPAA rating: ");
+
+    Console.WriteLine("Is this a classic? ");
+}
--- classwork/Section1/Section1/Program.cs
+++ classwork/Section1/Section1/Program.cs
@@ -21,7 +21,7 @@ bool isPassing;
 string name;
 string firstName = "Bob", lastName = "Smith ";
 
-firstName = lastName = name;
+//firstName = lastName = name;
 //firstName = "Sam";
 //lastName = "Sam";
 
@@ -31,9 +31,9 @@ firstName = lastName = name;
 //float fRate;
 
 //Block statement
-{
-    decimal payRate;
-}
+//{
+//    decimal payRate;
+//}
 
 //distanceFromMoon = 10.5;
 isPassing = distanceFromMoon > 0;
