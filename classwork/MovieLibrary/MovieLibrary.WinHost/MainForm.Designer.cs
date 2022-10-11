138a554870aa11199ef3fd44d0d159ce07cccdc0
michael.taylor <michael.taylor@my.tccd.edu>
2022-10-10 21:36:33 -0500

    Added child form to collect movie data. Added code to get movie details for new movie. Added helpers to display errors and confirmation.

--- classwork/MovieLibrary/MovieLibrary.WinHost/MainForm.Designer.cs
+++ classwork/MovieLibrary/MovieLibrary.WinHost/MainForm.Designer.cs
@@ -37,6 +37,7 @@
             this._miMovieDelete = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this._miHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this._lstMovies = new System.Windows.Forms.ListBox();
             this._mainMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,7 +64,7 @@
             // _miFileExit
             // 
             this._miFileExit.Name = "_miFileExit";
-            this._miFileExit.Size = new System.Drawing.Size(180, 22);
+            this._miFileExit.Size = new System.Drawing.Size(93, 22);
             this._miFileExit.Text = "E&xit";
             // 
             // moviesToolStripMenuItem
@@ -80,22 +81,23 @@
             // 
             this._miMovieAdd.Name = "_miMovieAdd";
             this._miMovieAdd.ShortcutKeys = System.Windows.Forms.Keys.Insert;
-            this._miMovieAdd.Size = new System.Drawing.Size(131, 22);
+            this._miMovieAdd.Size = new System.Drawing.Size(180, 22);
             this._miMovieAdd.Text = "&Add";
             this._miMovieAdd.Click += new System.EventHandler(this.OnMovieAdd);
             // 
             // _miMovieEdit
             // 
             this._miMovieEdit.Name = "_miMovieEdit";
-            this._miMovieEdit.Size = new System.Drawing.Size(131, 22);
+            this._miMovieEdit.Size = new System.Drawing.Size(180, 22);
             this._miMovieEdit.Text = "&Edit";
             // 
             // _miMovieDelete
             // 
             this._miMovieDelete.Name = "_miMovieDelete";
             this._miMovieDelete.ShortcutKeys = System.Windows.Forms.Keys.Delete;
-            this._miMovieDelete.Size = new System.Drawing.Size(131, 22);
+            this._miMovieDelete.Size = new System.Drawing.Size(180, 22);
             this._miMovieDelete.Text = "&Delete";
+            this._miMovieDelete.Click += new System.EventHandler(this._miMovieDelete_Click);
             // 
             // helpToolStripMenuItem
             // 
@@ -109,14 +111,24 @@
             // 
             this._miHelpAbout.Name = "_miHelpAbout";
             this._miHelpAbout.ShortcutKeys = System.Windows.Forms.Keys.F1;
-            this._miHelpAbout.Size = new System.Drawing.Size(180, 22);
+            this._miHelpAbout.Size = new System.Drawing.Size(126, 22);
             this._miHelpAbout.Text = "&About";
+            // 
+            // _lstMovies
+            // 
+            this._lstMovies.FormattingEnabled = true;
+            this._lstMovies.ItemHeight = 15;
+            this._lstMovies.Location = new System.Drawing.Point(12, 41);
+            this._lstMovies.Name = "_lstMovies";
+            this._lstMovies.Size = new System.Drawing.Size(776, 379);
+            this._lstMovies.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._lstMovies);
             this.Controls.Add(this._mainMenu);
             this.MainMenuStrip = this._mainMenu;
             this.Name = "MainForm";
@@ -139,5 +151,6 @@
         private ToolStripMenuItem _miMovieDelete;
         private ToolStripMenuItem helpToolStripMenuItem;
         private ToolStripMenuItem _miHelpAbout;
+        private ListBox _lstMovies;
     }
 }
