69cabca721093b42a402fb0bf6ee60a177ea09b3
michael.taylor <michael.taylor@my.tccd.edu>
2022-11-28 21:00:34 -0600

    Unit 9.2 - Finished adding DB commands - Added data reader to stream movies

--- classwork/MovieLibrary/MovieLibrary.Sql/SqlMovieDatabase.cs
+++ classwork/MovieLibrary/MovieLibrary.Sql/SqlMovieDatabase.cs
@@ -16,7 +16,36 @@ namespace MovieLibrary.Sql
             // IDisposable
             using (var conn = OpenConnection())
             {
-                throw new NotImplementedException();
+                //Create command option 2 - long way
+                var cmd = new SqlCommand();
+                cmd.CommandText = "AddMovie";
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.StoredProcedure; //CommandType.Text;
+
+                //Add parameters option 1 - best way
+                cmd.Parameters.AddWithValue("@name", movie.Title);
+
+                //Add parameters option 2 - long way (or with type)
+                var paramRating = new SqlParameter("@rating", movie.Rating);
+                cmd.Parameters.Add(paramRating);
+
+                //Add parameters option 3 - generic
+                var paramDescription = cmd.CreateParameter();
+                paramDescription.ParameterName = "@description";
+                paramDescription.Value = movie.Description;
+                paramDescription.DbType = DbType.String;
+                cmd.Parameters.Add(paramDescription);
+
+                cmd.Parameters.AddWithValue("@releaseYear", movie.ReleaseYear);
+                cmd.Parameters.AddWithValue("@runLength", movie.RunLength);
+                cmd.Parameters.AddWithValue("@isClassic", movie.IsClassic);
+
+                //Execute command and get result
+                object result = cmd.ExecuteScalar();
+                //movie.Id = (int)result;
+                movie.Id = Convert.ToInt32(result);
+
+                return movie;
             };
             
             #region try-finally equivalent
@@ -40,8 +69,29 @@ namespace MovieLibrary.Sql
         {
             using (var conn = OpenConnection())
             {
-                throw new NotImplementedException();
+                var cmd = new SqlCommand("FindByName", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@name", title);
+
+                //Read with streamed IO
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        return new Movie() {
+                            Id = (int)reader[0],              //Ordinal with cast
+                            Title = reader["Name"] as string, //Column name with cast
+                            Description = reader.GetString(2),//Typed name with ordinal
+                            Rating = reader.GetString("Rating"),
+                            RunLength = reader.GetInt32("RunLength"), //Typped name with column
+                            ReleaseYear = reader.GetFieldValue<int>("ReleaseYear"),
+                            IsClassic = reader.GetFieldValue<bool>("IsClassic")
+                        };
+                    };
+                };
             };
+
+            return null;
         }
 
         protected override IEnumerable<Movie> GetAllCore ()
@@ -54,12 +104,12 @@ namespace MovieLibrary.Sql
                 var cmd = new SqlCommand("GetMovies", conn);
 
                 //Need data adapter for Dataset
-                var da = new SqlDataAdapter(cmd);
+                var da = new SqlDataAdapter(cmd);                
 
                 //Buffered IO                
-                da.Fill(ds);
+                da.Fill(ds);                
             };
-
+            
             //Data loaded, can work with it now
             // Find table and then enumerate rows to get data
             var table = ds.Tables.OfType<DataTable>().FirstOrDefault();
@@ -84,22 +134,75 @@ namespace MovieLibrary.Sql
         {
             using (var conn = OpenConnection())
             {
-                throw new NotImplementedException();
+                var cmd = new SqlCommand("GetMovie", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", id);
+
+                //Read with streamed IO
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        return new Movie() {
+                            Id = (int)reader[0],              //Ordinal with cast
+                            Title = reader["Name"] as string, //Column name with cast
+                            Description = reader.IsDBNull(2) ? "" : reader.GetString(2),//Typed name with ordinal
+                            Rating = reader.GetString("Rating"),
+                            RunLength = reader.GetInt32("RunLength"), //Typped name with column
+                            ReleaseYear = reader.GetFieldValue<int>("ReleaseYear"),
+                            IsClassic = reader.GetFieldValue<bool>("IsClassic")
+                        };
+                    };
+                };
             };
+
+            return null;
         }
 
         protected override void RemoveCore ( int id )
         {
             using (var conn = OpenConnection())
             {
-                throw new NotImplementedException();
+                //Create command option 3 - generic
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "DeleteMovie";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+
+                //Set parameters
+                cmd.Parameters.AddWithValue("@id", id);
+
+                //Execute command 2 - no results/don't care
+                cmd.ExecuteNonQuery();
             };
         }
         protected override void UpdateCore ( int id, Movie movie )
         {
             using (var conn = OpenConnection())
             {
-                throw new NotImplementedException();
+                var cmd = new SqlCommand();
+                cmd.CommandText = "UpdateMovie";
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.StoredProcedure; //CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@name", movie.Title);
+                cmd.Parameters.AddWithValue("@rating", movie.Rating);
+                cmd.Parameters.AddWithValue("@description", movie.Description);
+                cmd.Parameters.AddWithValue("@releaseYear", movie.ReleaseYear);
+                cmd.Parameters.AddWithValue("@runLength", movie.RunLength);
+                cmd.Parameters.AddWithValue("@isClassic", movie.IsClassic);
+
+                //Execute command and get result
+                cmd.ExecuteNonQuery();
+
+                #region SQL Injection
+
+                //movie.Title = "SELECT * FROM Movies WHERE Name = '';DELETE FROM Movies;SELECT * FROM MOvies WHERE Name = ''";
+                //var cmd2 = new SqlCommand($"SELECT * FROM Movies WHERE Name = @title");
+                //cmd2.Parameters.AddWithValue("@title", movie.Title); 
+
+                #endregion
             };
         }
 
